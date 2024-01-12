5569b3594adcb06ac57b768a03c2c61bf9429d00
Mittaanjali <mitta.anjali@mphasis.com>
2024-01-12 18:29:46 +0530

    SchoolLibrary

---
+++ UnitTestProject/TeacherTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using SchoolLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class TeacherTest
+    {
+        [Test]
+        public void TestTeacherId()
+        {
+            var teacher = new Teacher();
+            teacher.TeacherId = 101;
+            Assert.AreEqual(101, teacher.TeacherId);
+        }
+        [Test]
+        public void TestTeacherName()
+        {
+            var teacher = new Teacher();
+            teacher.TeacherName = "Mrs.Sheela";
+            Assert.AreEqual("Mrs.Sheela", teacher.TeacherName);
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+
+}
+
